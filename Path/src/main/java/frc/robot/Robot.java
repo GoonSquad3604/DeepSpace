71c495f371ed49f6db90372ca432849b075702ef
chadrakoczy <chad.rakoczy@gmail.com>
2018-10-22 18:19:55 -0400

    Added motor objects and ids

--- Path/src/main/java/frc/robot/Robot.java
+++ Path/src/main/java/frc/robot/Robot.java
@@ -7,12 +7,31 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
   
+
+  public static WPI_TalonSRX leftMain;
+	public static WPI_TalonSRX rightMain;
+	public static WPI_TalonSRX leftSlave;
+	public static WPI_TalonSRX rightSlave;
+  
+
+
+
+
   @Override
   public void robotInit() {
+    
+    leftMain = new WPI_TalonSRX(15);
+		rightMain = new WPI_TalonSRX(1);
+		leftSlave = new WPI_TalonSRX(14);
+		rightSlave = new WPI_TalonSRX(2);
+		leftSlave.follow(leftMain);
+    rightSlave.follow(rightMain);
     
   }
 
@@ -21,7 +40,6 @@ public class Robot extends IterativeRobot {
   
   }
 
-  
   @Override
   public void autonomousInit() {
    
