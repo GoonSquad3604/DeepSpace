0f5b7ec240abc2c93a73b03df132d2a0b804b956
chadrakoczy <chad.rakoczy@gmail.com>
2019-01-08 07:44:34 -0500

    Name changes

--- DeepSpace/src/main/java/frc/robot/Constants.java
+++ DeepSpace/src/main/java/frc/robot/Constants.java
@@ -3,10 +3,10 @@ package frc.robot;
 public class Constants{
 
     //Talon IDs
-    public static int frontLeftID = 0;
-    public static int frontRightID = 0;
-    public static int rearRightID = 0;
-    public static int rearLeftID = 0;
+    public static int leftFrontID = 0;
+    public static int leftRearID = 0;
+    public static int rightFrontID = 0;
+    public static int rightRearID = 0;
 
     public static int elevatorID = 0;
 
--- DeepSpace/src/main/java/frc/subsystem/Elevator.java
+++ DeepSpace/src/main/java/frc/subsystem/Elevator.java
@@ -4,8 +4,9 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class Elevator extends WPI_TalonSRX{
 
-    public Elevator(int motorID){
-        super(motorID);
+    public Elevator(int elevatorID){
+        super(elevatorID);
     }
+
 
 }
