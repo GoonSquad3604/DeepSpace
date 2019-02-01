0c015ddb4905341bc7ca14bd94174b5a91c886ad
chadrakoczy <chad.rakoczy@gmail.com>
2019-02-01 16:24:01 -0500

    Uses DriveTrain

--- DeepSpace/src/main/java/frc/commands/drive/CmdDriveReverse.java
+++ DeepSpace/src/main/java/frc/commands/drive/CmdDriveReverse.java
@@ -1,7 +1,6 @@
 package frc.commands.drive;
 
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.commands.AutonCommand;
 import frc.subsystem.drivetrain.DriveTrain;
 
@@ -20,15 +19,9 @@ public class CmdDriveReverse implements AutonCommand
     }
     @Override
     public boolean isFinished() {
-        if(t.get()>time)
-        {
-            System.out.println("DON'T STOP ME NOOOOWWW...even though I will stop now");
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        
+        return t.get()>time;
+       
     }
 
     @Override
