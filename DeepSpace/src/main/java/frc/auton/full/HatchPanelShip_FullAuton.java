bfbcedc462acf2af5b98799d74e27f87fe5bf926
Lordoftheflame <anthonydalfonso15@gmail.com>
2019-02-23 12:05:19 -0500

    added paramaters comments

--- DeepSpace/src/main/java/frc/auton/full/HatchPanelShip_FullAuton.java
+++ DeepSpace/src/main/java/frc/auton/full/HatchPanelShip_FullAuton.java
@@ -10,10 +10,6 @@ public class HatchPanelShip_FullAuton
     public static void addCommands(Auton auton)
     {
         auton.addCommand(new CmdMoveToWaypoint(auton.getDrive(),null,"a")); //TODO specify waypoints
-<<<<<<< HEAD:DeepSpace/src/main/java/frc/auton/full/HatchPanelCargo_FullAuton.java
-        HatchPlaceAuton.addCommands(auton, kCargoLevelHatch);
-=======
         HatchPlaceAuton.addCommands(auton, kBottomRocketHatch);
->>>>>>> 1fdb2ca0fba7f5bc4607f435d7efdb52083d5fce:DeepSpace/src/main/java/frc/auton/full/HatchPanelShip_FullAuton.java
     }
 }
