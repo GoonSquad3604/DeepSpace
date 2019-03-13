1723df7b483ce39694047ade5d0d0f26e3f91a8d
GoonTurtle <truttle12@gmail.com>
2019-03-13 19:01:10 -0400

    Further added changes to automated hatch manipulator

--- DeepSpace/src/main/java/frc/auton/PickupPanelAuton.java
+++ DeepSpace/src/main/java/frc/auton/PickupPanelAuton.java
@@ -3,15 +3,32 @@ import static frc.robot.Constants.*;
 
 import frc.commands.special.CmdMerge;
 import frc.commands.subsystem.CmdMoveElevator;
+import frc.commands.subsystem.cargo.CmdMoveHinge;
 import frc.commands.drive.CmdManualDrive;
 public class PickupPanelAuton
 {
     public static void addCommands(Auton auton)
     {
+        //Sets Hinge to 0
+        auton.addCommand(new CmdMerge(
+            new CmdMoveHinge(0,1,auton.getCargoManipulator()),
+            new CmdManualDrive(auton.getDrive(),auton.getDriveStick(),auton.getOperateStick(),auton,false)
+        ));
+        //Deploy Hook
+        auton.addCommand(new CmdMerge(
+            null /*TODO replace with deploy hook*/,
+            new CmdManualDrive(auton.getDrive(),auton.getDriveStick(),auton.getOperateStick(),auton,false)
+        ));
+        //Slightly Moves Elevator
         double destination = auton.getElevator().getHeight() + kHatchMovementPickUp;
         auton.addCommand(new CmdMerge(
             new CmdMoveElevator(destination,auton.getElevator()),
-            new CmdManualDrive(auton.getDrive(),auton.getDriveStick(),auton.getOperateStick(),auton)
+            new CmdManualDrive(auton.getDrive(),auton.getDriveStick(),auton.getOperateStick(),auton,false)
+        ));
+        //Deploy Hook
+        auton.addCommand(new CmdMerge(
+            null /*TODO replace with retract hook*/,
+            new CmdManualDrive(auton.getDrive(),auton.getDriveStick(),auton.getOperateStick(),auton,false)
         ));
     } 
 }
